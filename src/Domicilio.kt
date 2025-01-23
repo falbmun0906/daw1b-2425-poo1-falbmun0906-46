327c377cb64e848d74e6a2a238162d668b37f3eb
falbmun0906 <amfrancisco@iesrafaelalberti.es>
2025-01-23 23:25:19 +0100

    Ejercicio 4.6 terminado.

--- src/Domicilio.kt
+++ src/Domicilio.kt
@@ -4,7 +4,21 @@
  * @param numero numero del domicilio
  * @constructor Crea un domicilio con calle y numero
  */
-class Domicilio(val calle: String, val numero: Int) {
+data class Domicilio(val calle: String, val numero: Int) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Domicilio) return false
+
+        return calle == other.calle && numero == other.numero
+    }
+
+    override fun hashCode(): Int {
+        var result = calle.hashCode()
+        result = 31 * result + numero
+        return result
+    }
+
     fun dirCompleta(): String {
         return "$calle, $numero"
     }
